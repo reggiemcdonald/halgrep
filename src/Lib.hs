7990039516482f21138264f90f7c3f34555b2cc7
kydas <kadasilva22@gmail.com>
2021-02-23 14:12:24 -0800

    Added args and option parsing, some simple print statements to show its working (#4)

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,73 @@ module Lib
     ( someFunc
     ) where
 
+import Options.Applicative
+
 someFunc :: IO ()
-someFunc = putStrLn "I'm afraid I can't do that."
+someFunc = run =<< execParser appInfo
+
+run :: Command -> IO ()
+run (Command (Opts c fz) (Args p fs)) = do
+  let files = map extractFile fs
+      patt = p
+  putStrLn $ "Files to search: " ++ foldr (\ x y -> x ++ " " ++ y) "" files
+  putStrLn $ "Pattern to match on: " ++ patt
+  putStrLn $ "Options: Context Lines: " ++ show c ++ " fuzzy level: " ++ fz
+
+data Opts = Opts
+  { optContext :: Int
+  , optFuzzy   :: String }
+
+context :: Parser Int
+context = option auto
+  (  long "context-lines"
+   <> short 'c'
+   <> metavar "NUM"
+   <> value 0
+   <> help "Print NUM lines preceding and following each matched line" )
+
+fuzzy :: Parser String
+fuzzy = strOption
+  ( long "fuzzy"
+  <> short 'f'
+  <> metavar "LEVEL"
+  <> value "NONE"
+  <> help "Desired LEVEL of fuzziness: NONE, LOW, MED, HIGH")
+
+
+opts :: Parser Opts
+opts = Opts <$> context <*> fuzzy
+
+
+data Args = Args
+  { argPattern :: String
+  , argFiles :: [String] }
+
+pattrn :: Parser String
+pattrn = argument str
+  ( metavar "PATTERN"
+  <> help "Pattern to search for")
+
+files :: Parser [String]
+files = some (argument str
+              ( metavar "FILES ..."
+              <> help "Files to search"))
+
+args :: Parser Args
+args = Args <$> pattrn <*> files
+
+data Command = Command Opts Args
+
+cmd :: Parser Command
+cmd = Command <$> opts <*> args
+
+appInfo :: ParserInfo Command
+appInfo = info (cmd <**> helper)
+  ( fullDesc
+  <> progDesc "Performs a search for the specified pattern in the given files."
+  <> header "halgrep - a text search utility" )
+
+
+findMatches lines startingLineNum regex = []
+extractFile path = path
+getFiles path = []
